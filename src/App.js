4a84ffc6f7a921b75d797983225849918561f381
sigfried <rssanjuan2704@gmail.com>
2020-10-10 20:29:50 -0500

    feat: Make use of the Trie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 
+import Suggestions from './components/Suggestions';
+
+import { useDebounce } from './common/hooks/';
+
+import Trie from './libs/Trie';
+
+const KEY_CODE = {
+  ENTER: 13,
+};
+
+const trie = new Trie();
+const DEBOUNCE = 500;
+
 function App() {
+  const inputRef = useRef(null);
+  const searchInputRef = useRef(null);
+
+  const [suggestions, setSuggestions] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('')
+  const debouncedSearchQuery = useDebounce(searchQuery, DEBOUNCE)
+
+  const insertString = () => {
+    const newString = inputRef.current.value;
+    trie.add(newString);
+
+    console.log(`Added ${newString}`);
+  }
+
+  const searchString = () => {
+    const searchValue = searchInputRef.current.value;
+    const result = trie.search(searchValue);
+    if (result) {
+      console.log(`String ${searchValue} found`);
+    }
+  }
+
+  const handleSubmit = (evt) => {
+    const { which} = evt;
+    if (which === KEY_CODE.ENTER) {
+      insertString();
+      inputRef.current.value = '';
+    }
+  }
+
+  const printTrie = () => {
+    trie.print();
+  }
+
+  useEffect(() => {
+    if (debouncedSearchQuery) {
+      const suggested = trie.suggestions(debouncedSearchQuery);
+      setSuggestions(suggested);
+    }
+
+  }, [debouncedSearchQuery])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <input
+            ref={inputRef}
+            onKeyDown={handleSubmit}
+            placeholder="Insert string"
+          />
+          <Suggestions
+            suggestions={suggestions}
+          />
+          <input
+            onChange={(evt) => setSearchQuery(evt.target.value)}
+            placeholder="String to search"
+          />
+          <button onClick={insertString}>Insert string</button>
+          <button onClick={searchString}>Search string</button>
+          <button onClick={printTrie}>Print trie</button>
+        </div>
+      </div>
     </div>
   );
 }
